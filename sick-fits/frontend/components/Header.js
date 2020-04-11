d436a1cb1e1c605505d186526ecf12c2e976371f
Richard See <richard.see@argos.co.uk>
2020-04-12 00:10:18 +1200

    Module 2.9

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,19 @@
-import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
+import NProgress from 'nprogress';
+import Nav from './Nav';
+import Router from 'next/router';
+
+// NProgress styles set in Meta.js
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
