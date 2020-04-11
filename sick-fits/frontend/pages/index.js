1c1d212dbe7187731438a0031c3ee35702221a83
Richard See <richard.see@argos.co.uk>
2020-04-11 16:54:40 +1200

    Module 1.4

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+// No need to import React, Next.js handles it automatically
+// Link bound to HTML5 push state
+import Link from 'next/link';
+
+const Home = () => (
+  <div>
+    <p>Hey!</p>
+  </div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+// No need to import React, Next.js handles it automatically
+import Link from 'next/link'
+
+const Sell = () => (
+  <div>
+    <p>Sell!</p>
+  </div>
+);
+
+export default Sell;
