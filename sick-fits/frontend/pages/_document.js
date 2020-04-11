42b16746251fc7f57943b8d1d84c2bfa4118f394
Richard See <richard.see@argos.co.uk>
2020-04-12 00:17:03 +1200

    Module 2.9

---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    // Collects all styles required for first render
+    // Walks the component tree to find all the necessary styles
+    // THIS GETS RID OF STYLE FLICKERING ON FIRST RENDER
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
