1c627b55feeee5beae7462895cdc47fa2db6ccfd
Richard See <richard.see@argos.co.uk>
2020-04-13 01:46:07 +1200

    Module 3.13

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+// Where database calls are going to be made, regardless of what DB you are using
+const Mutations = {
+  // Each GraphQL request comes in, you get four variables
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+// Where database calls are going to be made, regardless of what DB you are using
+const Query = {
+  // Each GraphQL request comes in, you get four variables
+  dogs(parent, args, context, info) {
+    // return [{ name: 'Snickers' }, { name: 'Sonny' }];
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
