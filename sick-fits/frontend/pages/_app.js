867ac0c9842f0145558666213cb4178cc5d515f7
Richard See <richard.see@argos.co.uk>
2020-04-13 22:40:35 +1200

    Module 4.15

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,19 +1,40 @@
 // Custom App.js. Next makes  one automatically but can be overridden with this custom file
 import App, { Container } from 'next/app';
 import Page from '../components/Page'
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+  // Special Next.js function. Will run before first render
+  // ctx = context
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    // Every single page that we have, will crawl for any queries or mutations
+    // that need to be fetched and resolve them before the page renders
+    // This is NOT necessary for client side rendered apps, only for server side (Next)
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    };
+
+    // Exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render () {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} /> {/* Pass fetched pageProps down to entire app */}
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
