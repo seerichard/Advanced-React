8d96abaefbce851861427d8909664080cfae84b0
Richard See <richard.see@argos.co.uk>
2020-06-29 00:13:14 +1200

    Module 6.39

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,6 +1,7 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -13,6 +14,34 @@ function createClient({ headers }) {
         headers,
       });
     },
+    // Local state data
+    clientState: {
+      resolvers: {
+        Mutation: {
+          // Underscore used if param not used - General practice to do this
+          // Destructured { cache } was from client - client: { cache }
+          toggleCart(_, variables, { cache }) {
+            // Read the cartOpen value from the cache
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY
+            });
+
+            // Write the cart state to the opposite
+            const data = {
+              data: { cartOpen: !cartOpen }
+            };
+
+            // Write to the cache
+            cache.writeData(data);
+
+            return data;
+          }
+        }
+      },
+      defaults: {
+        cartOpen: false
+      }
+    }
   });
 }
 
