5ee3d4c1118acce4dccb357ba3fb1f604f60593f
Richard See <richard.see@argos.co.uk>
2020-07-26 01:32:49 +1200

    Module 7.48

--- sick-fits/frontend/components/Search.js
+++ sick-fits/frontend/components/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Router from 'downshift';
-import Downshift from 'downshift';
+import Router from 'next/router';
+import Downshift, { resetIdCounter } from 'downshift';
 import { ApolloConsumer } from 'react-apollo';
 import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
@@ -20,6 +20,15 @@ const SEARCH_ITEMS_QUERY = gql`
     }
   }
 `;
+
+const routeToItem = (item) => {
+  Router.push({
+    pathname: '/item',
+    query: {
+      id: item.id
+    }
+  })
+}
 
 class AutoComplete extends React.Component {
   state = {
@@ -46,34 +55,62 @@ class AutoComplete extends React.Component {
   );
 
   render() {
+    const { loading, items } = this.state;
+
+    resetIdCounter(); // For aria warning
+
     return (
       <SearchStyles>
-        <div>
-          {/* Exposes the Apollo client */}
-          <ApolloConsumer>
-            {client => (
-              <input
-                type="search"
-                onChange={e => {
-                  e.persist(); // Allows persistence across async events
-                  this.onChange(e, client);
-                }}
-              />
-            )}
-          </ApolloConsumer>
-          <DropDown>
-            {this.state.items.map(({ id, image, title }) => (
-              <DropDownItem key={id} >
-                <img
-                  width="50"
-                  src={image}
-                  alt={title}
-                />
-                {title}
-              </DropDownItem>
-            ))}
-          </DropDown>
-        </div>
+        <Downshift
+          onChange={routeToItem}
+          itemToString={item => (item === null ? '' : item.title)}
+        >
+          {({ getInputProps, getItemProps, isOpen, inputValue, highlightedIndex }) => (
+            <div>
+              {/* Exposes the Apollo client */}
+              <ApolloConsumer>
+                {client => (
+                  <input
+                    {...getInputProps({
+                      id: 'search',
+                      type: "search",
+                      placeholder: 'Search For An Item',
+                      className: loading ? 'loading': undefined,
+                      onChange: e => {
+                        e.persist(); // Allows persistence across async events
+                        this.onChange(e, client);
+                      }
+                    })}
+                  />
+                )}
+              </ApolloConsumer>
+              {/* isOpen adds functionality for when you click away from dropdown to auto close */}
+              {isOpen && (
+                <DropDown>
+                  {items.map((item, index) => (
+                    <DropDownItem
+                      key={item.id}
+                      {...getItemProps({ item })}
+                      highlighted={index === highlightedIndex}
+                    >
+                      <img
+                        width="50"
+                        src={item.image}
+                        alt={item.title}
+                      />
+                      {item.title}
+                    </DropDownItem>
+                  ))}
+                  {!items.length && !loading && (
+                    <DropDownItem>
+                      Nothing found for {inputValue}
+                    </DropDownItem>
+                  )}
+                </DropDown>
+              )}
+            </div>
+          )}
+        </Downshift>
       </SearchStyles>
     )
   }
