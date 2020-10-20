199a2b30ab73fc68987fc54d4ccbd7fede495a6b
Richard See <richard.see@argos.co.uk>
2020-10-20 20:10:16 +0100

    Module 9.57

---
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -0,0 +1,49 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A Cool Item',
+  price: 5000,
+  description: 'This item is really cool!',
+  image: 'dog.jpg',
+  largeImage: 'largeDog.jpg'
+};
+
+describe('<Item/>', () => {
+  it('Renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+
+    // Debug the actual HTML using debug()
+    // console.log(wrapper.debug());
+
+    const img = wrapper.find('img');
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it('Renders the price tag and title properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+
+    // Find the <a/> inside the <Title/> component
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+    
+    const PriceTag = wrapper.find('PriceTag');
+    expect(PriceTag.children().text()).toBe('$50');
+  });
+
+  it('Renders the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+
+    const buttonList = wrapper.find('.buttonList');
+    expect(buttonList.children()).toHaveLength(3);
+    
+    // All three equivalent
+    expect(buttonList.find('Link')).toHaveLength(1);
+    expect(buttonList.find('Link').exists()).toBe(true);
+    expect(buttonList.find('Link').exists()).toBeTruthy();
+
+    expect(buttonList.find('AddToCart').exists()).toBe(true);
+    expect(buttonList.find('DeleteItem').exists()).toBe(true);
+  });
+});
